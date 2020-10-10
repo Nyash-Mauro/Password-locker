ac5f3464132691ee4555ccb5a3cdae26c08f544c
Nyash-Mauro <mauricee423@gmail.com>
2020-10-10 10:31:00 +0300

    added method to return account name

--- user.py
+++ user.py
@@ -6,19 +6,27 @@ class User:
     user_list = [] #empty user list
 
     def __init__(self,account_name,login_username,user_password):
-        '''Initializing the var'''
+        '''
+        Initializing the var
+        '''
         self.account_name = account_name
         self.login_username = login_username
         self.user_password = user_password
 
     def save_user(self):
-        '''Method that saves user in the user_list'''
+        '''
+        Method that saves user in the user_list
+        '''
         User.user_list.append(self)
 
     def delete_user(self):
-        """ method that deletes the saved user """
+        """
+         method that deletes the saved user
+        """
         User.user_list.delete(self)
     
     @classmethod
     def find_by_account_name(cls,account_name):
-        
+        """ 
+        This method returns the user that matches the account name
+        """
