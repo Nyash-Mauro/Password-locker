eda48db2eb307098082bb4b7cdc58dc4b4f76d90
Nyash-Mauro <mauricee423@gmail.com>
2020-10-10 11:15:44 +0300

    added test user class

--- test_user.py
+++ test_user.py
@@ -1,2 +1,15 @@
 import unittest #importing unittest from python
 from user import User #importing class user
+
+class TestUser(unittest.TestCase):
+    """
+     This test defines test cases for user class behaviors
+
+     Args:
+          unittest.TestCase: TestCase that aids in creating test cases
+    """
+
+    def setUp(self):
+        """ 
+        this method to run bef
+        """
