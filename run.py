7491e50015c20a559d0743b46f8e9e076ca73033
Nyash-Mauro <mauricee423@gmail.com>
2020-10-10 23:09:03 +0300

    added function to create a new user

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 import random
 from user import User
+
+
+def create_user(account, username, password):
+    """
+    Function to create a new user for the list
+    """
+    new_user = User(account, username, password)
+    return new_user
